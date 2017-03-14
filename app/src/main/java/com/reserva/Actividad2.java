e0e23f083baa786d4f30ecf1d4439afea715f380
Alejandro Tejeda <alextejedab@gmail.com>
2017-03-14 16:26:59 -0600

    Initial commit

---
+++ app/src/main/java/com/reserva/Actividad2.java
@@ -0,0 +1,48 @@
+package com.reserva;
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Actividad2 extends Activity {
+
+	String nombre = "", fecha = "", hora = "", nombreD = "", nombreR = "", celebras = "";
+	int personas = 0;
+	TextView muestraDatos;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.actividad2);
+
+		muestraDatos = (TextView) findViewById(R.id.muestraDatos);
+
+		Bundle recibe = new Bundle();
+		recibe = this.getIntent().getExtras();
+
+		nombre = recibe.getString("nombre");
+		personas = recibe.getInt("personas");
+		fecha = recibe.getString("fecha");
+		hora = recibe.getString("hora");
+		nombreD = recibe.getString("nombreD");
+		nombreR = recibe.getString("nombreR");
+		celebras = recibe.getString("celebras");
+
+		muestraDatos.setText("Reservacion a nombre de:\n" + nombre + "\n" + personas
+				+ " personas\nFecha: " + fecha + "\nHora: " + hora + "\n" + "nombre Restaurante:\n" + nombreR + "nombre Dueño:\n" + nombreD + "¿Que celebras?:\n" + celebras);
+
+	}
+
+    public void hacerOtraReserva(View v) {
+        Intent envia = new Intent(this, MainActivity.class);
+        finish();
+        startActivity(envia);
+    }
+
+}
